3f51a5236eddcfb6137ed398b3331f65b63629b4
TharaniBaskar <84002480+TharaniBaskar@users.noreply.github.com>
2021-05-12 19:17:22 +0530

    Add files via upload

---
+++ op.py
@@ -0,0 +1,8 @@
+a=int(input("enter the value"));
+b=int(input("enter the value"));
+c=a+b;
+print("the addition is",c)
+c=a-b;
+print("the subraction is",c)
+c=a*b;
+print("the multiplication is",c)
