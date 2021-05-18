bf2c299244fdd6000ccfba196ea7d4288b6681f7
TharaniBaskar <84002480+TharaniBaskar@users.noreply.github.com>
2021-05-18 19:44:57 +0530

    Delete op.py

--- op.py
+++
@@ -1,8 +0,0 @@
-a=int(input("enter the value"));
-b=int(input("enter the value"));
-c=a+b;
-print("the addition is",c)
-c=a-b;
-print("the subraction is",c)
-c=a*b;
-print("the multiplication is",c)
